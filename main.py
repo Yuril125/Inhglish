3f90c4c527a2962f052c9d9b14c1c03370e88e56
Yuril <YurilAvali@protonmail.com>
2023-04-13 12:54:14 -0400

    IPA transcription

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+import eng_to_ipa as ipa
+
+
+def main() -> None:
+    english = input("Enter English: ").split()
+    transcription = [ipa.convert(word, keep_punct=False, retrieve_all=True) for word in english]
+    print("\n".join(str(n) for n in transcription))
+
+
+if __name__ == "__main__":
+    while True:
+        main()
